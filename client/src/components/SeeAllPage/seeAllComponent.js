e4b03bbf14ce0f6823c510392a077d3dd7d89c8a
humbejeff2116 <humbejeff2116@gmail.com>
2021-03-13 14:41:56 +0000

    added a view button and modified styles to SeeAll component

--- client/src/components/SeeAllPage/seeAllComponent.js
+++ client/src/components/SeeAllPage/seeAllComponent.js
@@ -22,28 +22,44 @@ import logo from '../../images/logo.png'
 //  the ProductItem component, which we will be used to render each product on the product list.
  const SeeAllComp =(props) => {
        
-  const view = (src,name,price,id,available) => {
+  const view = (src,name,price,description,id,available) => {
     let item =[];
-      item.push({ src, name, price, id,available });
+      item.push({ src, name, price,description, id,available });
      localStorage.setItem('view', JSON.stringify(item));
 
   }
 
-  const {src,name,price,available,id} = props
+  const {src,name,price,description,available,id} = props
  
         return (
           <ErrorBoundary>
 
-            <div className="all-items-picture"  onClick={()=>view(src,name,price,id,available)} >
+            <div className="all-items-picture"  onClick={()=>view(src,name,price,description,id,available)} >
               
                 <div className="see-all-items-details">
                 <Link to="/view-item">
                 <img src={logo} width="80%" height="80%" alt="img"/><br />            
                 <span className="product-title"><small>Name: {name}</small></span><br />
                 <span className="product-price"><small>Price: ${price}</small></span><br />
-                { (!available) ?<span className="text-danger"> product is out of stock </span>:'' } 
                 </Link>
                 </div> 
+                { 
+                  (!available) ?
+
+                  <div className="product-bttn">
+                  <p className="product-text-danger"> 
+                    *Out of stock* 
+                  </p>
+                  </div> : <div className="product-bttn">
+                              <Link to="/view-item" >
+                                <button  onClick={()=>view(src,name,price,description,id,available)} >
+                                    {/* <i> <FaRegEye className="contact-bttn-icon"/></i> */}
+                                  View 
+                                </button>
+                              </Link>
+                              </div> 
+
+                } 
 
             </div>
             </ErrorBoundary>
